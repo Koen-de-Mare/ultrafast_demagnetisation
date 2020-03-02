5849a3110391dab9f9c581496b38eb5c63588494
koen <k.a.d.mare@student.tue.nl>
2020-03-02 16:18:28 +0100

    changed animate.py to work with the changes made in the previous commit

--- src/animate.py
+++ src/animate.py
@@ -3,14 +3,15 @@ import math
 import matplotlib.animation as animation
 
 from src.phyiscs import *
+from src.system_parameters.default_system import make_default_system
+from src.simulation_parameters.default_simulation import make_default_simulation
 
-#system = make_equilibrium(300, 1.25, 160)
-#system = make_equilibrium(300, 2.5, 80)
-system = make_equilibrium(300, 5.0, 40)
-#system = make_equilibrium(300, 10.0, 20)
+syspar = make_default_system()
+simpar = make_default_simulation()
 
-dt: float = 0.5
-num_frames: int = round(150 / dt)
+system = make_equilibrium(syspar, simpar)
+
+num_frames: int = round(150 / simpar.dt)
 
 include_ballistic = False  # include magnetisation of ballistic electrons in moke
 
@@ -44,40 +45,40 @@ for i in range(num_frames):
         0.5 * (t - t_pulse)*(t_pulse - t) / (pulse_duration * pulse_duration)) / (pulse_duration * math.sqrt(2.0 * math.pi))
     # (eV fs^-1 nm^-2)
 
-    system = system.step(dt, fluence)
-    t += dt
+    system = system.step(fluence)
+    t += simpar.dt
 
 print(system.extra_electrons())
 
-steps = [x * system.sliceLength for x in range(0, system.num_slices)]
+steps = [x * simpar.sliceLength for x in range(0, system.num_slices)]
 
 # code based on:
 # https://stackoverflow.com/questions/49165233/two-lines-matplotib-animation
 
 fig, (ax1, ax2, ax3, ax4) = plt.subplots(4, 1, figsize=(10, 8))
 
-ax1.set_xlim(0, system.sliceLength * system.num_slices)
+ax1.set_xlim(0, simpar.sliceLength * system.num_slices)
 ax1.set_ylim(-0.01, 0.01)
 ax1.set_ylabel("mu up \n (eV)")
 ax1.axhline(y=0, color='b')
 line1, = ax1.plot([], [], lw=3)
 line2, = ax1.plot([], [], lw=3)
 
-ax2.set_xlim(0, system.sliceLength * system.num_slices)
+ax2.set_xlim(0, simpar.sliceLength * system.num_slices)
 ax2.set_ylim(-0.01, 0.01)
 ax2.set_ylabel("mu dn \n (eV)")
 ax2.axhline(y=0, color='b')
 line3, = ax2.plot([], [], lw=3)
 line4, = ax2.plot([], [], lw=3)
 
-ax3.set_xlim(0, system.sliceLength * system.num_slices)
+ax3.set_xlim(0, simpar.sliceLength * system.num_slices)
 ax3.set_ylim(-0.5, 0.5)
 ax3.set_ylabel("magnetisation \n (nm^-3)")
 ax3.axhline(y=0, color='b')
 line5, = ax3.plot([], [], lw=3)
 
 
-ax4.set_xlim(0, system.sliceLength * system.num_slices)
+ax4.set_xlim(0, simpar.sliceLength * system.num_slices)
 ax4.set_ylim(-0.1, 0.1)
 ax4.set_ylabel("spin current \n (nm^-2 fs^-1)")
 ax4.axhline(y=0, color='b')
@@ -98,14 +99,14 @@ def init():
 
 def animate(n):
     line1.set_data(steps, mu0_up_lists[n])
-    line2.set_data(steps, list(map((lambda x: x / Ds_up), excited_up_lists[n])))
+    line2.set_data(steps, list(map((lambda x: x / syspar.Ds_up), excited_up_lists[n])))
     line3.set_data(steps, mu0_dn_lists[n])
-    line4.set_data(steps, list(map((lambda x: x / Ds_dn), excited_dn_lists[n])))
+    line4.set_data(steps, list(map((lambda x: x / syspar.Ds_dn), excited_dn_lists[n])))
 
     magnetisation = [0.0] * system.num_slices
     for i in range(system.num_slices):
         magnetisation_thermal = \
-            mu0_up_lists[n][i] * Ds_up - mu0_dn_lists[n][i] * Ds_dn
+            mu0_up_lists[n][i] * syspar.Ds_up - mu0_dn_lists[n][i] * syspar.Ds_dn
         magnetisation_nonthermal = \
             excited_up_lists[n][i] - excited_dn_lists[n][i]
 
@@ -118,7 +119,7 @@ def animate(n):
     return [line1, line2, line3, line4, line5, line6]
 
 
-anim = animation.FuncAnimation(fig, animate, init_func=init, frames=num_frames, interval=dt*100, blit=True)
+anim = animation.FuncAnimation(fig, animate, init_func=init, frames=num_frames, interval=simpar.dt*100, blit=True)
 
 show = True
 if show:
